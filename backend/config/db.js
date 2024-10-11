5f440e8cfd46130e398be8b281b79df78a9eabe8
Joe2G <youssefwaleed178@outlook.com>
2024-10-11 18:47:23 +0300

    modified:   backend/config/db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_PASSWORD, {
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
-  dialect: 'mysql2',
+  dialect: 'mysql',
   logging: false,
   pool: {
     max: 5,
