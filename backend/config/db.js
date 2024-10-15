83b80d8bcbd4240b3b21a306871fcb8f356c1955
Joe2G <youssefwaleed178@outlook.com>
2024-10-15 20:10:41 +0300

    modified:   backend/config/db.js 	modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   vercel.json

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,28 +1,31 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+// Initialize Sequelize with PostgreSQL (Supabase uses Postgres)
 const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  dialect: 'mysql',
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',  // Set the dialect to postgres
+    logging: false,  // Disable logging; set to true if you want Sequelize logs
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
-});
+);
+
 // Database connection function
 async function dbConnect() {
   try {
     await sequelize.authenticate();
     console.log('Database connection has been established successfully.');
   } catch (error) {
-    // Enhanced error handling
+    // Enhanced error handling for different database connection errors
     if (error instanceof Sequelize.ConnectionError) {
       console.error('Database connection error:', error.message);
     } else if (error instanceof Sequelize.HostNotFoundError) {
