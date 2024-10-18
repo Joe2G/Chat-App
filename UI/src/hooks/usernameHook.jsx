a900edd7816ea75a84708eaaed11d6f11b9de60b
Joe2G <youssefwaleed178@outlook.com>
2024-10-18 19:38:13 +0300

    modified:   UI/src/hooks/usernameHook.jsx

--- UI/src/hooks/usernameHook.jsx
+++ UI/src/hooks/usernameHook.jsx
@@ -50,7 +50,7 @@ export default function useUsernameHook() {
           })
             .then(response => {
               if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error(`Network response was not ok: ${response.statusText}`);
               }
               return response.json();
             })
